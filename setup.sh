4bae71343ce7cffeef57753d4b90f8040d419140
Tim Evans <evans.tim@gmail.com>
2022-11-13 20:51:07 +0000

    Updated setup.sh to include first few AUTH0 params

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 export DATABASE_URL="postgresql://tim@localhost:5432/capstone"
-export EXCITED="true"
-echo "setup.sh script executed successfully!"
+export FLASK_APP="app.py"
+export AUTH0_DOMAIN="tim-eu.eu.auth0.com"
+export API_AUDIENCE="agency"
+export AUTH0_CALLBACK_URL="http://0.0.0.0:8080" 
